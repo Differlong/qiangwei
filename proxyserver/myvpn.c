6362c67a19c9714b70ee01a31b3c956be981f336
Telanx <1241818518@qq.com>
2016-06-16 15:21:15 +0800

    Update myvpn.c

--- proxyserver/myvpn.c
+++ proxyserver/myvpn.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include "mysql.h"
 #define IEM_dbhost "localhost"
-#define IEM_dbuser "root"
-#define IEM_dbpass "xnalet"
+#define IEM_dbuser "telanx"
+#define IEM_dbpass "telanx"
 #define IEM_dbname "qwt"
 #define IEM_dbtable "account"
 #define IEM_db_name_col "uid"
@@ -21,44 +21,43 @@ int main(){
         exit(0);
     }
     while(1){
-        if (fgets(Inbuf, IEM_io_buffer, stdin) == NULL)
+        if (fgets(Inbuf, IEM_io_buffer, stdin) == NULL) {
             printf("ERR\n");
-            if ((p = strchr(Inbuf,'\n')) != NULL)
-                *p = '\0';          /* strip /n */
-            if ((p = strchr(Inbuf, ' ')) == NULL) {
-                printf("ERR\n");
-                continue;
-            }
-            *p++ = '\0';
-            break;
+        }
+        if ((p = strchr(Inbuf,'\n')) != NULL) {
+            *p = '\0';          /* strip /n */
+        }
+        if ((p = strchr(Inbuf, ' ')) == NULL) {
+            printf("ERR\n");
+            continue;
+        }
+        *p++ = '\0';
+        break;
     }
-                            /* varlidate the user via mysql database */
-                            sprintf(Sqlbuf,"select "IEM_db_name_col" from "IEM_dbtable" where "IEM_db_name_col"='%s' and "IEM_db_pass_col"='%s' and expiredate>=current_date",Inbuf,p );
-                                mysql=mysql_init(NULL);
-                                if(mysql==NULL){
-                                            printf("ERR\n");
-
-                                }
-                                if(mysql_real_connect(mysql,IEM_dbhost,IEM_dbuser,IEM_dbpass,IEM_dbname,0,NULL,0)==NULL){
-                                            printf("ERR\n");
-
-                                }
-                                if(mysql_query(mysql,Sqlbuf)!=0){
-                                            printf("ERR\n");
-
-                                }
-                                if(!(res=mysql_store_result(mysql))){
-                                            printf("ERR\n");
-                                                    mysql_close(mysql);
-
-                                }
-                                    if(res->row_count==1)
-                                                printf("OK\n");
-                                        else
-                                                    printf("ERR\n");
-                                            mysql_free_result(res);
-                                                mysql_close(mysql);
-                                                    return 1;
-
+    
+    /* 查询mysql */
+    sprintf(Sqlbuf,"select "IEM_db_name_col" from "IEM_dbtable" where "IEM_db_name_col"='%s' and "IEM_db_pass_col"='%s' and expiredate>=current_date",Inbuf,p );
+    mysql=mysql_init(NULL);
+    if(mysql==NULL){
+        printf("ERR\n");
+    }
+    if(mysql_real_connect(mysql,IEM_dbhost,IEM_dbuser,IEM_dbpass,IEM_dbname,0,NULL,0)==NULL){
+        printf("ERR\n");
+    }
+    if(mysql_query(mysql,Sqlbuf)!=0){
+        printf("ERR\n");
+     }
+    if(!(res=mysql_store_result(mysql))){
+        printf("ERR\n");
+        mysql_close(mysql);
+    }
+    if(res->row_count==1) {
+        printf("OK\n");
+    } else {
+        printf("ERR\n");
+    }
+    mysql_free_result(res);
+    mysql_close(mysql);
+    return 1;
+    
 }
-
